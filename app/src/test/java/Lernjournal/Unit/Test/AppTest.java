00d215f434d8db2af0699a9c89ba2e720d8e98d1
cavicphi <cavicphi@students.zhaw.ch>
2022-05-08 12:41:47 +0200

    Schritt 22 (Code Anpassungen durch cavicphi)

--- app/src/test/java/Lernjournal/Unit/Test/AppTest.java
+++ app/src/test/java/Lernjournal/Unit/Test/AppTest.java
@@ -10,53 +10,53 @@ class AppTest {
     @Test 
         public void BussenConverter_keineBusse (){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Keine Busse da keine Geschwindigkeitsüberschreitung", busse.convert(0));
+        Assertions.assertEquals("Keine Busse da keine Geschwindigkeitsüberschreitung", busse.convert(-10));
     }
     @Test
         public void Bussenconverter_Bussevon20(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Busse von CHF 20", busse.convert(5));
+        Assertions.assertEquals("Busse von CHF 20", busse.convert(3));
         }
     @Test 
         public void Bussenconverter_Bussevon60(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Busse von CHF 60", busse.convert(10));
+        Assertions.assertEquals("Busse von CHF 60", busse.convert(7));
         }
     @Test
         public void Bussenconverter_Bussevon120(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Busse von CHF 120", busse.convert(13));
+        Assertions.assertEquals("Busse von CHF 120", busse.convert(11));
         }
     @Test
         public void Bussenconverter_Bussevon180(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Busse von CHF 180", busse.convert(16));
+        Assertions.assertEquals("Busse von CHF 180", busse.convert(20));
         }
     @Test
         public void Bussenconverter_Bussevon260(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Busse von CHF 260", busse.convert(23));
+        Assertions.assertEquals("Busse von CHF 260", busse.convert(21));
         }
     @Test
         public void Bussenconverter_AnzeigemitVerwarnung(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals("Anzeige mit Verwarnung", busse.convert(29));
+        Assertions.assertEquals("Anzeige mit Verwarnung", busse.convert(30));
         }
     @Test
         public void Bussenconverter_AnzeigeFüE1M(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals ("Anzeige und Führerausweisentzug von mind. 1 Monat", busse.convert(33));
+        Assertions.assertEquals ("Anzeige und Führerausweisentzug von mind. 1 Monat", busse.convert(32));
         }
     @Test
         public void Bussenconverter_AnzeigeFüE3M(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals ("Anzeige und Führerausweisentzug von mind. 3 Monaten", busse.convert(76));
+        Assertions.assertEquals ("Anzeige und Führerausweisentzug von mind. 3 Monaten", busse.convert(75));
         
         }
      
         @Test
         public void Bussenconverter_Raserdelikt(){
         Bussenconverter busse = new Bussenconverter();
-        Assertions.assertEquals ("Raserdelikt Anzeige, Führerausweisentzug für mind. 2 Jahre, Fahreignungs-Untersuchung", busse.convert(83));
+        Assertions.assertEquals ("Raserdelikt Anzeige, Führerausweisentzug für mind. 2 Jahre, Fahreignungs-Untersuchung", busse.convert(180));
         }
 }
