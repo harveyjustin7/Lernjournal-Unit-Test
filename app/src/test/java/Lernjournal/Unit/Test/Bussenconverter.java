b7042eb8c4d1b2851ed3fabd38f2371ac662cd2b
harveyjustin7 <92264933+harveyjustin7@users.noreply.github.com>
2022-05-08 07:39:40 +0200

    Schritt 8 (Code Erweiterung)

--- app/src/test/java/Lernjournal/Unit/Test/Bussenconverter.java
+++ app/src/test/java/Lernjournal/Unit/Test/Bussenconverter.java
@@ -5,8 +5,11 @@ package Lernjournal.Unit.Test;
         if (i>0 && i<6){
             return "Busse von CHF 20";
         }
-        if (i>6){
+        if (i>=6 && i<11){
             return "Busse von CHF 60";
+        }
+        if (i>=10 && i<16){
+            return "Busse von CHF 120";
         }
             return "Keine Busse";
     }
